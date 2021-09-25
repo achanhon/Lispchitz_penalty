7679cbe09b9eab8aeae38e208270a693836b721b
teletravail <adrienchanhonton@gmail.com>
2021-09-25 17:38:50 +0200

    70% en train et 10 en test ????
    il s'est passé quoi ???

--- detection/test.py
+++ detection/test.py
@@ -51,7 +51,7 @@ print("load data")
 import dataloader
 
 if whereIam == "super" or True:
-    cia = dataloader.CIA(flag="custom", custom=["isprs/test"])
+    cia = dataloader.CIA(flag="custom", custom=["isprs/train"])
 else:
     cia = dataloader.CIA("test")
 
@@ -93,13 +93,13 @@ with torch.no_grad():
                 debug = image[0].cpu().numpy()
                 debug = numpy.transpose(debug, axes=(1, 2, 0))
                 debug = PIL.Image.fromarray(numpy.uint8(debug))
-                debug.save("build/" + town[0:-5] + "_" + str(i) + "_x.png")
+                debug.save("build/" + str(i) + "_x.png")
                 debug = label.cpu().numpy() * 255
                 debug = PIL.Image.fromarray(numpy.uint8(debug))
-                debug.save("build/" + town[0:-5] + "_" + str(i) + "_y.png")
+                debug.save("build/" + str(i) + "_y.png")
                 debug = pred.cpu().numpy() * 255
                 debug = PIL.Image.fromarray(numpy.uint8(debug))
-                debug.save("build/" + town[0:-5] + "_" + str(i) + "_z.png")
+                debug.save("build/" + str(i) + "_z.png")
 
         cm[town] = cm[town].cpu().numpy()
         g, pre, rec = dataloader.perf(cm[town])
