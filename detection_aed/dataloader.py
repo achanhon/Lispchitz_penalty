fa7002ad6d4e662849552ef8f4d05a33134225af
teletravail <adrienchanhonton@gmail.com>
2021-10-13 14:54:12 +0200

    +++++

--- detection_aed/dataloader.py
+++ detection_aed/dataloader.py
@@ -143,6 +143,5 @@ if __name__ == "__main__":
     batchloader = aed.getbatchloader(nbtiles=0, batchsize=8)
     x, y = next(iter(batchloader))
 
-    for i in range(8):
-        torchvision.utils.save_image(x[i] / 255, "build/cropimage" + str(i) + ".png")
-        torchvision.utils.save_image(y[i], "build/croplabel" + str(i) + "s.png")
+    torchvision.utils.save_image(x / 255, "build/cropimage.png")
+    torchvision.utils.save_image(y.unsqueeze(1).float(), "build/croplabel.png")
