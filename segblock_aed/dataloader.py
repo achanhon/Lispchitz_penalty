2bd00f07a41d83ff605ca6354cbd5d0d51848bd6
achanhon <adrien.chan_hon_tong@onera.fr>
2021-10-15 13:44:22 +0200

    ++

--- segblock_aed/dataloader.py
+++ segblock_aed/dataloader.py
@@ -18,8 +18,7 @@ def symetrie(x, y, i, j, k):
     return x.copy(), y.copy()
 
 
-
-def computeperf(yz=None,stat=None):
+def computeperf(yz=None, stat=None):
     assert yz is not None or stat is not None
     if stat is not None:
         good, fa, miss = stat[0], stat[1], stat[2]
@@ -31,33 +30,12 @@ def computeperf(yz=None,stat=None):
             recall = good / (good + miss)
         return torch.Tensor([precision * recall, precision, recall])
     else:
-        y,z = yz
-        good = torch.sum((y==1).float()*(z>0).float())
-        fa = torch.sum((y==0).float()*(z>0).float())
-        miss = torch.sum((y==1).float()*(z<=0).float())
+        y, z = yz
+        good = torch.sum((y == 1).float() * (z > 0).float())
+        fa = torch.sum((y == 0).float() * (z > 0).float())
+        miss = torch.sum((y == 1).float() * (z <= 0).float())
         return torch.Tensor([good, fa, miss])
-        
 
-def resize(x,h,w):
-	globalresize = torch.nn.AdaptiveAvgPool2d((h, w))
-	
-	np = ("numpy" in str(type(x)))
-	if np:
-		x = torch.Tensor(x)
-		
-	remove = (len(x.shape)==2)
-	if remove:
-		x = x.unsqueeze(0)
-		
-	x = globalresize(x)
-	
-	if remove:
-		x = x[0]
-	
-	if np:
-		x = x.cpu().numpy()
-	return x
-	
 
 class AED:
     def __init__(self, flag):
@@ -86,11 +64,11 @@ class AED:
 
         image = PIL.Image.open(self.root + name + ".jpg").convert("RGB")
         image = np.uint8(np.asarray(image).copy())
-        
+
         h, w = image.shape[0], image.shape[1]
         h2, w2 = (h // 64) * 64, (w // 64) * 64
         globalresize = torch.nn.AdaptiveAvgPool2d((h2, w2))
-        
+
         tmp = np.transpose(image, axes=(2, 0, 1))
         tmp = torch.Tensor(tmp)
         tmp = globalresize(tmp)
@@ -99,8 +77,8 @@ class AED:
         mask = np.zeros((image.shape[0], image.shape[1]))
         points = self.labels[name]
         for c, r in points:
-            mask[int(r*h2/h)][int(c*w2/w)] = 1
- 
+            mask[int(r * h2 / h)][int(c * w2 / w)] = 1
+
         if torchformat:
             x = torch.Tensor(np.transpose(image, axes=(2, 0, 1)))
             return x.unsqueeze(0), torch.Tensor(mask)
@@ -116,12 +94,18 @@ class AED:
             image, label = self.getImageAndLabel(name, torchformat=False)
 
             # random crop
-            row = np.random.randint(0, label.shape[0] - tilesize//16 - 2, size=NB)
-            col = np.random.randint(0, label.shape[1] - tilesize//16 - 2, size=NB)
+            row = np.random.randint(0, label.shape[0] - tilesize // 16 - 2, size=NB)
+            col = np.random.randint(0, label.shape[1] - tilesize // 16 - 2, size=NB)
 
             for i in range(NB):
-                im = image[row[i]*16 : row[i]*16 + tilesize, col[i]*16 : col[i]*16 + tilesize, :]
-                mask = label[row[i] : row[i] + tilesize//16, col[i] : col[i] + tilesize//16]
+                im = image[
+                    row[i] * 16 : row[i] * 16 + tilesize,
+                    col[i] * 16 : col[i] * 16 + tilesize,
+                    :,
+                ]
+                mask = label[
+                    row[i] : row[i] + tilesize // 16, col[i] : col[i] + tilesize // 16
+                ]
                 XY.append((im.copy(), mask.copy()))
 
             # positive crop
@@ -130,16 +114,20 @@ class AED:
                 l = [(row[i], col[i]) for i in range(row.shape[0])]
                 random.shuffle(l)
                 l = l[0 : min(len(l), nbpos)]
-                noise = np.random.randint(-tilesize//16, tilesize//16, size=(len(l), 2))
+                noise = np.random.randint(
+                    -tilesize // 16, tilesize // 16, size=(len(l), 2)
+                )
 
                 for i, (r, c) in enumerate(l):
                     R, C = r + noise[i][0], c + noise[i][1]
 
-                    R = max(0, min(R, label.shape[0] - tilesize//16 - 2))
-                    C = max(0, min(C, label.shape[1] - tilesize//16 - 2))
+                    R = max(0, min(R, label.shape[0] - tilesize // 16 - 2))
+                    C = max(0, min(C, label.shape[1] - tilesize // 16 - 2))
 
-                    im = image[R*16 : R*16 + tilesize, C*16 : C*16 + tilesize, :]
-                    mask = label[R : R + tilesize//16, C : C + tilesize//16]
+                    im = image[
+                        R * 16 : R * 16 + tilesize, C * 16 : C * 16 + tilesize, :
+                    ]
+                    mask = label[R : R + tilesize // 16, C : C + tilesize // 16]
                     XY.append((im.copy(), mask.copy()))
 
         # symetrie
@@ -176,9 +164,16 @@ if __name__ == "__main__":
     debug = np.transpose(debug, axes=(1, 2, 0))
     debug = PIL.Image.fromarray(np.uint8(debug))
     debug.save("build/image.png")
-    debug = mask.cpu().numpy()
-    debug = PIL.Image.fromarray(np.uint8(debug) * 255)
+    globalresize = torch.nn.AdaptiveAvgPool2d((debug.shape[0], debug.shape[1]))
+    debug = globalresize(mask.unsqueeze(0).float())
+    debug = debug[0].cpu().numpy() * 255
+    debug = PIL.Image.fromarray(numpy.uint8(debug))
     debug.save("build/label.png")
 
+    loader = aed.getbatchloader(nbtiles=0)
+    x, y = next(iter(loader))
+
     torchvision.utils.save_image(x / 255, "build/cropimage.png")
-    torchvision.utils.save_image(y.unsqueeze(1).float(), "build/croplabel.png")
+    globalresize = torch.nn.AdaptiveAvgPool2d((x.shape[2], x.shape[3]))
+    y = globalresize(y.float())
+    torchvision.utils.save_image(y.unsqueeze(1), "build/croplabel.png")
