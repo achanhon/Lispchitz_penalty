9bbaf5384801319d9ca131fe3ee491a8fba02d4e
teletravail <adrienchanhonton@gmail.com>
2021-10-17 10:54:09 +0200

    ++

--- thread_segblock_aed/run.py
+++ thread_segblock_aed/run.py
@@ -22,9 +22,13 @@ if not os.path.exists(root + "AED"):
 os.system("rm -rf build")
 os.makedirs("build")
 
+os.system("cat train.py") # force nohup to flush
+
 if whereIam == "ldtis706z":
     os.system("python3 train.py")
     os.system("python3 test.py")
 if whereIam in ["calculon", "astroboy", "flexo", "bender"]:
     os.system("/d/jcastillo/anaconda3/bin/python train.py")
     os.system("/d/jcastillo/anaconda3/bin/python test.py")
+
+os.system("cat train.py") # force nohup to flush
